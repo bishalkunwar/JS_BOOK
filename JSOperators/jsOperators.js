d824037c77f03ad92d3bf383e6f21e722958618c
Bishal Kunwar <bishal.kunwar487@gmail.com>
2023-07-16 00:11:13 -0400

    getting started with the JS Operators

--- JSOperators/jsOperators.js
+++ JSOperators/jsOperators.js
@@ -1,61 +1,77 @@
-// JavaScript operators operate the operands, these are symbols that are used to manipulate a certain value or operand. Operators are used to performing specific mathematical and logical computations on operands. In other words, we can say that an operator operates the operands. In JavaScript, operators are used to compare values, perform arithmetic operations, etc.
+// // JavaScript operators operate the operands, these are symbols that are used to manipulate a certain value or operand. Operators are used to performing specific mathematical and logical computations on operands. In other words, we can say that an operator operates the operands. In JavaScript, operators are used to compare values, perform arithmetic operations, etc.
 
-// JavaScript Operators: There are various operators supported by JavaScript.
+// // JavaScript Operators: There are various operators supported by JavaScript.
 
-// JS Arithmetic Operators
-// JS Assignment Operators
-// JS Comparison Operators
-// JS Logical Operators
-// JS Ternary Operators
-// JS Bitwise Operators
-// JS typeof Operator 
+// // JS Arithmetic Operators
+// // JS Assignment Operators
+// // JS Comparison Operators
+// // JS Logical Operators
+// // JS Ternary Operators
+// // JS Bitwise Operators
+// // JS typeof Operator 
 
 
-// Arithmetic Operators: the operators that operate upon the numerical values and return a numerical value.
+// // Arithmetic Operators: the operators that operate upon the numerical values and return a numerical value.
 
-// Addition Operator
-let add = 6+9;
-console.log(add);
+// // Addition Operator
+// let add = 6+9;
+// console.log(add);
 
-// Subtraction Operator
-let sub = 10-7;
-console.log(sub);
+// // Subtraction Operator
+// let sub = 10-7;
+// console.log(sub);
 
-// Multiplication Operator
-let result = 3*3;
-console.log(result);
+// // Multiplication Operator
+// let result = 3*3;
+// console.log(result);
 
-// Division Operator
-let div = 2.0/2/0;
-console.log(div);
+// // Division Operator
+// let div = 2.0/2/0;
+// console.log(div);
 
-// Modulas Operator
-let rem = 9%5;
-console.log(rem);
+// // Modulas Operator
+// let rem = 9%5;
+// console.log(rem);
 
-// Exponentian Operator
-let pow = 2**3;
-console.log(pow);
+// // Exponentian Operator
+// let pow = 2**3;
+// console.log(pow);
 
-// Increment Operator
-let g = 2;
-let g1 = g++; 
-console.log(g);
+// // Increment Operator
+// let g = 2;
+// let g1 = g++; 
+// console.log(g);
 
-// Decrement Operator
-let num1 = 5;
-let num2 = num1--;
+// // Decrement Operator
+// let num1 = 5;
+// let num2 = num1--;
+// console.log(num1);
+
+// // Unary Plus Operator
+// let num6 = 24;
+// num7 = +num6;
+// console.log(num7); 
+
+// // Negation Operator
+// let num4 = 56;
+// num5 = -num4;
+// console.log(num5);
+
+
+
+
+// Assaignment Operator: The assignment operation evaluates the assigned value. Chaining the assignment operator is possible in order to assign a single value to multiple variables
+
+// Assaignment Operator
+let num1 = 3;
 console.log(num1);
 
-// Unary Plus Operator
-let num6 = 24;
-num7 = +num6;
-console.log(num7); 
+// Addition Assaignment Operator
+const a = 4;
+console.log(num1=a+3);
 
-// Negation Operator
-let num4 = 56;
-num5 = -num4;
-console.log(num5);
+// Subtraction Assaignment Operator
+let yaho = 8;
+console.log(foho = yaho-2);
 
-
-
+//
