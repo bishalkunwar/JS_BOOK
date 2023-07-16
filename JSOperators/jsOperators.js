bd3449a21c4d30bfb6245e107c9990712c763109
Bishal Kunwar <bishal.kunwar487@gmail.com>
2023-07-15 23:20:13 -0400

    JS Operator Examples

---
+++ JSOperators/jsOperators.js
@@ -0,0 +1,61 @@
+// JavaScript operators operate the operands, these are symbols that are used to manipulate a certain value or operand. Operators are used to performing specific mathematical and logical computations on operands. In other words, we can say that an operator operates the operands. In JavaScript, operators are used to compare values, perform arithmetic operations, etc.
+
+// JavaScript Operators: There are various operators supported by JavaScript.
+
+// JS Arithmetic Operators
+// JS Assignment Operators
+// JS Comparison Operators
+// JS Logical Operators
+// JS Ternary Operators
+// JS Bitwise Operators
+// JS typeof Operator 
+
+
+// Arithmetic Operators: the operators that operate upon the numerical values and return a numerical value.
+
+// Addition Operator
+let add = 6+9;
+console.log(add);
+
+// Subtraction Operator
+let sub = 10-7;
+console.log(sub);
+
+// Multiplication Operator
+let result = 3*3;
+console.log(result);
+
+// Division Operator
+let div = 2.0/2/0;
+console.log(div);
+
+// Modulas Operator
+let rem = 9%5;
+console.log(rem);
+
+// Exponentian Operator
+let pow = 2**3;
+console.log(pow);
+
+// Increment Operator
+let g = 2;
+let g1 = g++; 
+console.log(g);
+
+// Decrement Operator
+let num1 = 5;
+let num2 = num1--;
+console.log(num1);
+
+// Unary Plus Operator
+let num6 = 24;
+num7 = +num6;
+console.log(num7); 
+
+// Negation Operator
+let num4 = 56;
+num5 = -num4;
+console.log(num5);
+
+
+
