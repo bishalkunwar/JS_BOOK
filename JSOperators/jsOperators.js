3f645f0ab7111e2284b6d0f4bd3679138dc21e83
Bishal Kunwar <bishal.kunwar487@gmail.com>
2023-07-18 10:36:28 -0400

    Different Types of JS Operations
    different JS Operands and Operations like mathematical arithmetical and assaignment

--- JSOperators/jsOperators.js
+++ JSOperators/jsOperators.js
@@ -13,65 +13,160 @@
 
 // // Arithmetic Operators: the operators that operate upon the numerical values and return a numerical value.
 
-// // Addition Operator
-// let add = 6+9;
-// console.log(add);
+// Addition Operator
+let add = 6+9;
+console.log(add);
 
-// // Subtraction Operator
-// let sub = 10-7;
-// console.log(sub);
+// Subtraction Operator
+let sub = 10-7;
+console.log(sub);
 
-// // Multiplication Operator
-// let result = 3*3;
-// console.log(result);
+// Multiplication Operator
+let result = 3*3;
+console.log(result);
 
-// // Division Operator
-// let div = 2.0/2/0;
-// console.log(div);
+// Division Operator
+let div = 2.0/2/0;
+console.log(div);
 
-// // Modulas Operator
-// let rem = 9%5;
-// console.log(rem);
+// Modulas Operator
+let rem = 9%5;
+console.log(rem);
 
-// // Exponentian Operator
-// let pow = 2**3;
-// console.log(pow);
+// Exponentian Operator
+let pow = 2**3;
+console.log(pow);
 
-// // Increment Operator
-// let g = 2;
-// let g1 = g++; 
-// console.log(g);
+// Increment Operator
+let g = 2;
+let g1 = g++; 
+console.log(g);
 
-// // Decrement Operator
-// let num1 = 5;
-// let num2 = num1--;
-// console.log(num1);
-
-// // Unary Plus Operator
-// let num6 = 24;
-// num7 = +num6;
-// console.log(num7); 
-
-// // Negation Operator
-// let num4 = 56;
-// num5 = -num4;
-// console.log(num5);
-
-
-
-
-// Assaignment Operator: The assignment operation evaluates the assigned value. Chaining the assignment operator is possible in order to assign a single value to multiple variables
-
-// Assaignment Operator
-let num1 = 3;
+// Decrement Operator
+let num1 = 5;
+let num2 = num1--;
 console.log(num1);
 
-// Addition Assaignment Operator
-const a = 4;
-console.log(num1=a+3);
+// Unary Plus Operator
+let num6 = 24;
+num7 = +num6;
+console.log(num7); 
 
-// Subtraction Assaignment Operator
-let yaho = 8;
-console.log(foho = yaho-2);
+// Negation Operator
+let num4 = 56;
+num5 = -num4;
+console.log(num5);
 
-//
+
+
+// // Assaignment Operator: The assignment operation evaluates the assigned value. Chaining the assignment operator is possible in order to assign a single value to multiple variables
+
+// // Assaignment Operator
+// let num1 = 3;
+// console.log(num1);
+
+// // Addition Assaignment Operator
+// const a = 4;
+// console.log(num1=a+3);
+
+// // Subtraction Assaignment Operator
+// let yaho = 8;
+// console.log(foho = yaho-2);
+
+// // Multiplication Assignment
+
+     
+
+// // Division Assignment Operator
+// const moo=14;
+// let yoo = 2;
+// console.log(yoo=yoo/moo);
+  
+// // Modulas Assignment Operator
+// console.log(yoo%=2);
+ 
+// // Exponentian Assignment Operator
+// console.log(yoo**moo);
+ 
+// // Left Shift Assignment
+// console.log(yoo<<=2);
+ 
+// // Right Shift Assignment
+// console.log(yoo>>=2);
+  
+// // Bitwise AND Assignment
+// console.log(yoo&=2);
+  
+// // Bitwise OR Assignment
+// console.log(yoo|=2);
+ 
+// // Bitwise XOR Assignment
+// console.log(yoo^=2);
+
+
+
+
+// // Comparison Operator:
+
+// // Assigning values
+// let num1 = 5;
+// let num2 = 5;
+// // Equality Operator
+// console.log(num1 == num2);
+
+// // Strict equality Operator
+// console.log(num1 === num2);
+
+// // Inequality Operator
+// console.log(num1 != num2);
+
+// // Strict Inequality Operator
+// console.log(num1 !== num2);
+
+// // Greater than Operator
+// console.log(num1 > num2);
+
+// // Greater than or equal Operator
+// console.log(num1 >= num2);
+
+// // Less than Operator
+// console.log(num1 < num2);
+
+// // Less than or equal Operator
+// console.log(num1 <= num2); 
+
+
+
+// // Assigning values
+// let val1 = 5;
+// let val2 = 7;
+    
+// // Logical !(NOT) Operator
+// console.log(!val2);
+
+// // Logical &&(AND) Operator
+// console.log( val1 && val2 );
+    
+// // Lgical ||(OR) Operator
+// console.log( val1 || val2 );
+
+
+// // Ternary Operator 
+// // Assigning values
+// let PMarks = 40
+    
+// // Ternary Operator
+// let result = (PMarks > 39)? "Pass":"Fail";
+// console.log(result); 
+
+
+// let a = 17;
+// let b = "GeeksforGeeks";
+// let c = "";
+// let d = null;
+ 
+// console.log("Type of a = " + (typeof a));
+// console.log("Type of b = " + (typeof b));
+// console.log("Type of c = " + (typeof c));
+// console.log("Type of d = " + (typeof d));
+// console.log("Type of e = " + (typeof e));
