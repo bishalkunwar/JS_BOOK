84a3f17c588140ca8a30fb91f9258e6c3db44d72
Bishal Kunwar <bishal.kunwar487@gmail.com>
2023-09-02 09:29:52 -0400

    Add files via upload

---
+++ Numbers/numberObject.js
@@ -0,0 +1,18 @@
+// Number Objects in JS
+
+let num1 = 10;
+let num2 = 12;
+
+console.log(num1);
+console.log(Number(num1));
+
+console.log(num1 == Number(num1));
+
+
+// Properties of JavaScript Numbers:
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.POSITIVE_INFINITY);
+
+console.log(isNaN(num1));
---
+++ Numbers/numbersInJS.js
@@ -0,0 +1,18 @@
+ // Javascript numbers are primitive data types, unlike other programming languages, we do not need int float etc to declare the numeric values in JS, JS numbers are always stored in double precision 64 bit binary format.
+ // 0-51 bits stores value (Fraction), 52-62 bit stores exponent and 63 bit stores sign.
+
+ 
+
+ // Different Number systems:
+ // Octal -- let x = 0123; // if the number starts with 0 and if any digit is less or smaller than 8, then that is parsed as an octal number.
+// Binary -- let x = 0b11; // binary numbers starts with ob or OB and followws only 0 and 1.
+// Hexadecimal -- let x = 0xfff // they starts with 0x or 0X followed by any digits from zero to 9 and a to f.
+// Decimal -- 0-9; // eventhough we use decimal number system with computing, the OS converts it into binary notations for computing.
+
+
+
+
+// Note: what is the major difference between signed and unsigned intigers, though computer basically follows unsigned intigers,
+// if computer follows 2's compliment to store the signed intigers, when do we use the signed intigers. According to my understanding, if any number is negative inorder to make it positive we do follow 2's compliment so is that the concept of singed intigers.
+
+// so can i understand positive decimal numbers as unsigned intigers and negative to signed intigers? 
