1f256d4769dc181bdeadbbae125902b654766aae
Bishal Kunwar <bishal.kunwar487@gmail.com>
2023-09-02 09:59:06 -0400

    Basic Math Objects and Methods in JS

---
+++ Math/basicMathObjects.js
@@ -0,0 +1,18 @@
+// The JS Math Objects is used to perform mathematical operations on numbers:
+// import Math;
+
+console.log("Some Math Objects are as follows:-- ")
+
+console.log(`Math Natural Logarithm of 10:- LN10:- ${Math.LN10}`);
+console.log(`Math Natural Logarithm of 2:- LN10:- ${Math.LN2}`);
+console.log(`Math  static data property represents the base 2 logarithm of e:- ${Math.LOG2E}`);
+console.log(`Math PI Object:- ${Math.PI}`);
+console.log(`Math  SQRT2:- ${Math.SQRT2}`);
+
+
+
+console.log(`Absolute value of -4.7:- ${Math.abs(-4.7)}`);
+console.log(`Ceil Value of 4.3 is:- ${Math.ceil(4.3)}`);
+console.log(`floor value of 4.7 is:- ${Math.floor(4.7)}`);
+console.log(`Math.sin(90 * Math.PI / 180):- ${Math.sin(90*Math.PI/180)}`);
+console.log(`Minimum value of (20, 34, 12, 25, 67) is:- ${Math.min(20, 34, 12, 25, 67)}`);
---
+++ Math/mathObjectsUseCase.js
@@ -0,0 +1,25 @@
+// Math is an inbuilt object that has attributes and methods for mathematical functions and constants. It’s not a function object. 
+// Math object works with the Number type. The Math object does not have a constructor. All properties and methods of Math are fixed/static. The cosine function is known as Math.cos(y) while the constant pi is known as Math.PI, where y is the method’s argument. All the properties and methods of Math are static and can be called by using Math as an object without creating it.
+
+
+
+console.log("Math.PI :" + Math.PI);
+console.log("Math.SQRT2:" + Math.SQRT2);
+console.log("Math.SQRT1_2:" + Math.SQRT1_2);
+console.log("Math.LN10:" + Math.LN10);
+console.log("Math.LN2:" + Math.LN2);
+console.log("Math.LOG10E:" + Math.LOG10E);
+console.log("Math.LOG2E:" + Math.LOG2E);
+
+
+
+
+console.log("ceil :" + Math.ceil(9.6));
+console.log("floor :" + Math.floor(9.6));
+console.log("round :" + Math.round(9.6));
+console.log("sine :" + Math.sin(30));
+console.log("cosine :" + Math.cos(30));
+console.log("min :" + Math.min(30, 40));
+console.log("max :" + Math.max(30, 40));
+console.log("sign :" + Math.sign(-40));
+console.log("abs :" + Math.sign(-40));
