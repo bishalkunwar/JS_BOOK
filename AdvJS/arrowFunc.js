3f26fa488edc5854b8910df3358c9a80c24bbe62
Bishal Kunwar <bishal.kunwar487@gmail.com>
2023-09-08 11:43:51 -0400

    Arrow Functions in JS

---
+++ AdvJS/arrowFunc.js
@@ -0,0 +1,17 @@
+// // Arrow Functions => Arrow Functions are the anonymous functions i.e. function without the name and without the identifier bounded.
+// // Arrow functions do not return any value and can be declared without the function keyword.
+// // Arrow functions are also called Lambda Functions.
+
+// // Syntax:=> 
+// const func1 = (a,b)=>{
+
+// }
+
+
+// Arrow functions without the parameter:
+const tryFunc = () => console.log("Hellow, this is arrow function.");
+tryFunc();
+
+// with parameter:
+const tryFunc1 = (a,b) => console.log(a+b);
+tryFunc1(1,2);
