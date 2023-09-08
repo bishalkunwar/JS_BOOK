ee64a28f7ef0aa183b9161b13a899c091afa8e86
Bishal Kunwar <bishal.kunwar487@gmail.com>
2023-09-08 09:55:29 -0400

    Boolean Data Type in JS

---
+++ BooleanJS/boolean.js
@@ -0,0 +1,12 @@
+// Boolean data types returns the value either true or false.
+// Boolean is used as a function to get the value of a variable, object, conditions, expressions, etc in terms of true or false.
+// 1 is True and 0 is False Value.
+
+let var1 = true;
+let var2 = new Boolean(true);
+let var3 = 1;
+let var4 = 3;
+
+
+console.log(var3 === var4);
+console.log(var1 === var2);
